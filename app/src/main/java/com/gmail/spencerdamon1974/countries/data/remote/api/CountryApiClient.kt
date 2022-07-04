e98055e88c27b47e733b8df20b8a8a4a72c3c030
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-04 00:10:36 -0700

    Create Retrofit object instance to use the interface

--- app/src/main/java/com/gmail/spencerdamon1974/countries/data/remote/api/CountryApiClient.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/countries/data/remote/api/CountryApiClient.kt
@@ -1,4 +1,15 @@
 package com.gmail.spencerdamon1974.countries.data.remote.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object CountryApiClient {
+    private const val BASE_URL = "https://restcountries.com/v3.1/"
+    val retrofitInstance: CountryApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CountryApi::class.java)
+    }
 }
