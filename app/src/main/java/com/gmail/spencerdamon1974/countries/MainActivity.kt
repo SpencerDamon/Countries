192b667fb123ab904f8f06eae09d78c10adaf212
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-06 15:02:54 -0700

    Create MainActivity Coroutine and test Pass

--- app/src/main/java/com/gmail/spencerdamon1974/countries/MainActivity.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/countries/MainActivity.kt
@@ -2,15 +2,31 @@ package com.gmail.spencerdamon1974.countries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.gmail.spencerdamon1974.countries.data.remote.respository.CountryRepository
 import com.gmail.spencerdamon1974.countries.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val countryRepository = CountryRepository()
+    private lateinit var countryAdapter: CountryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val countries = countryRepository.getCountries().toList()
+            Log.d("Countries", countries.toString())
+        }
     }
+
 }
