2c9bc9ee9e80cbf9608f6b9d5f3d353c09f600a4
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-06 15:23:39 -0700

    jump to main thread initialize recyclerview - complete

--- app/src/main/java/com/gmail/spencerdamon1974/countries/MainActivity.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/countries/MainActivity.kt
@@ -3,11 +3,13 @@ package com.gmail.spencerdamon1974.countries
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import com.gmail.spencerdamon1974.countries.data.remote.respository.CountryRepository
 import com.gmail.spencerdamon1974.countries.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,10 +25,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setupLoadingState()
         CoroutineScope(Dispatchers.IO).launch {
             val countries = countryRepository.getCountries().toList()
+            countryAdapter = CountryAdapter(countries)
             Log.d("Countries", countries.toString())
+            withContext(Dispatchers.Main) {
+                setupRecyclerView()
+                setupSuccessState()
+            }
+
         }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = countryAdapter
+    }
+
+    private fun setupSuccessState() = with(binding) {
+        recyclerview.isVisible = true
+        progressLoading.isVisible = false
+    }
+
+    private fun setupLoadingState() = with(binding){
+        recyclerview.isVisible = false
+        progressLoading.isVisible = true
     }
 
 }
